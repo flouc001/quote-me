13ea1c58c8d22fecde31104d350a072d2c35f28b
flouc001 <flouc001@gmail.com>
2020-05-12 16:57:01 +0100

    add styled comp, add highcharts

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
+/* eslint-disable react/jsx-filename-extension */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import WebFont from 'webfontloader';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+
+import theme from './theme';
 import App from './App';
+
+import './index.css';
+
 import * as serviceWorker from './serviceWorker';
+
+WebFont.load({
+  google: {
+    families: ['Roboto'],
+  },
+});
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    color: ${(props) => props.theme.colors.white};
+    background-color: ${(props) => props.theme.colors.black};
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/theme.js
@@ -0,0 +1,12 @@
+// https://www.amc.com/shows/mad-men
+
+const theme = {
+  colors: {
+    red: '#ed3023',
+    white: '#fff',
+    black: '#000',
+    grey: '#1a1a1a',
+  },
+};
+
+export default theme;
